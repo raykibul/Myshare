8512b4820da26ca221121beae59e6545e1b44dc5
Rakibul islam <rakibcuet16@gmail.com>
2020-01-13 12:51:46 +0600

    Initial commit

---
+++ myshare/src/main/java/coom/rakibul/myshare/Myshare.java
@@ -0,0 +1,15 @@
+package coom.rakibul.myshare;
+
+import android.content.Context;
+
+import java.net.ContentHandler;
+
+public class Myshare {
+    static  Context mycontext;
+
+    public static void initialize(Context context){
+         mycontext=context;
+    }
+
+
+}
